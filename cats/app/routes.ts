ecee8f0ed483d633a68d041a9c4087ae85f575b9
khanaccount1 <khanaccount1@mail.ru>
2025-01-17 15:25:55 +0700

    fix

--- cats/app/routes.ts
+++ cats/app/routes.ts
@@ -1,5 +1,8 @@
 import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
-    layout("routes/layout.tsx", [index("routes/all-cats.tsx"), route("favorite-cats", "routes/favorite-cats.tsx")]),
+    layout("routes/layout.tsx", [
+        index("routes/all-cats.tsx"),
+        route("favorite-cats", "routes/favorite-cats.tsx"),
+    ]),
 ] satisfies RouteConfig;
--- cats/vite.config.ts
+++ cats/vite.config.ts
@@ -1,18 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    css: {
-        postcss: {
-            plugins: [tailwindcss, autoprefixer],
-        },
-        modules: {
-            scopeBehaviour: "local",
-        },
+    build: {
+        outDir: "dist",
     },
-
+    server: {
+        // Specify the development server port
+        port: 3001,
+    },
+    // Base name of your app
+    base: "/",
     plugins: [reactRouter(), tsconfigPaths()],
 });
