7ec11c32e9cab7238bdbf94d40da5e73cbe160ab
khanaccount1 <khanaccount1@mail.ru>
2025-01-17 21:54:08 +0700

    fix baseUrl

--- cats/vite.config.ts
+++ cats/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/frontend-challenge/",
+    base: "./assets/",
     plugins: [react()],
     resolve: {
         alias: {
